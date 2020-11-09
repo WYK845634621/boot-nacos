695cd434cadf939db8fc2283ff9a7c8d6eb70f78
WYK845634621 <1263928214@qq.com>
2020-11-09 11:26:14 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/comtroller/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.demo.comtroller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Tips
+ * @Author yikai.wang
+ * @Number K1171305
+ * @Date 2020/11/6 14:13
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/get")
+    public String get() {
+        return useLocalCache + ";  port is:" + port;
+    }
+}
